791f6d150cb1a50814ca269bba411aec88f12600
kobarmn <kout.kobayashi@gmail.com>
2026-04-16 21:45:06 +0900

    2026/04/16 入力画面作成

---
+++ lib/input_form.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class InputForm extends StatefulWidget {
+  const InputForm({super.key});
+
+  @override
+  State<InputForm> createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            maxLines: 5,
+            decoration: const InputDecoration(
+              hintText: '文章を入力してください',
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(onPressed: () {}, child: Text('変換'))
+      ],
+    );
+  }
+}
