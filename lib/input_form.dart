059ed77ce2771e7662f698d81772c50a6cfbe4fe
kobarmn <kout.kobayashi@gmail.com>
2026-04-20 21:10:05 +0900

    Json リクエスト・レスポンスオブジェクトの定義

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -8,23 +8,53 @@ class InputForm extends StatefulWidget {
 }
 
 class _InputFormState extends State<InputForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  // TextFiled Widgetにて入力された値を取得、変更する機能を持つ。
+  final _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            maxLines: 5,
-            decoration: const InputDecoration(
-              hintText: '文章を入力してください',
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
+              controller: _textEditingController,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                hintText: '文章を入力してください',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  print('文章が入力されいません。');
+                  return '文章が入力されていません';
+                }
+                return null;
+              },
             ),
           ),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(onPressed: () {}, child: Text('変換'))
-      ],
+          const SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () {
+                final formState = _formKey.currentState!;
+                if (!formState.validate()) {
+                  return;
+                }
+                debugPrint('text = ${_textEditingController}');
+              },
+              child: Text('変換'))
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
